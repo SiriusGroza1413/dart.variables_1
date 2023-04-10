65a6dc456a444bc2eabf5479fa1ed20656ada579
Guillermo Ojeda <gro1413@hotmail.com>
2023-04-10 12:01:13 -0300

    first commit

---
+++ bin/variables_1.dart
@@ -0,0 +1,16 @@
+void main(List<String> arguments) {
+
+  //variable que contiene el nombre de una persona
+  var nombre = "Luis";
+
+  //variable que contiene la edad de una persona
+  var edad = 29;
+
+  var primerApellido = "Garcia";
+
+  /* las dos lineas de abajo sirven para identificar el tipo de un valor
+  guardado en una varibale usando el metodo runtimeType
+  */
+  print(nombre.runtimeType);
+  print(edad.runtimeType);
+}
